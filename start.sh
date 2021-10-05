9d58dcc8b0c5d43d31242a873a53f7314252d39b
Job Vink <jmcvink@gmail.com>
2021-10-05 17:06:07 +0200

    added cleaning to the script

--- start.sh
+++ start.sh
@@ -8,6 +8,9 @@ echo 'loading modules'
 module load python/3.6.0
 module load prun
 
+echo 'clearning up previous runs'
+rm -r CODE_PATH/../results/result*
+
 # wait one second for the command to actually reserve the nodes
 sleep 1
 preserve -llist

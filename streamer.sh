408583b39dcfba8bab0444b9a899209c238a8c64
Job Vink <jmcvink@gmail.com>
2021-10-05 11:27:04 +0200

    updated host name on streamer

--- streamer.sh
+++ streamer.sh
@@ -1,3 +1,4 @@
 cd /var/scratch/ddps2105/ddps1/
 sh setup.sh
-/var/scratch/ddps2105/Python-3.9.7/python streamer.py --host ${workers[0]}.cm.cluster &!
+echo "starting streamer on $1"
+/var/scratch/ddps2105/Python-3.9.7/python streamer.py --host $1.cm.cluster &!

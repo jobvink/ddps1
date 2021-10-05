009a64404fae579518c323c43ae92238ea0fa3f9
Job Vink <jmcvink@gmail.com>
2021-10-05 11:18:27 +0200

    updated worker now has spark path

--- worker.sh
+++ worker.sh
@@ -1,4 +1,9 @@
 cd /var/scratch/ddps2105/ddps1/
 sh setup.sh
+
+# configure spark
+export SPARK_HOME=/var/scratch/ddps2105/spark-3.1.1-bin-hadoop3.2
+export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
+
 echo "starting worker for spark://$1.cm.cluster:7077"
 $SPARK_HOME/sbin/start-worker.sh spark://"$1".cm.cluster:7077 &
